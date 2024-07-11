68d6444d970b283e8a9ed6bd0661057ba1e096a7
vinay <vinaythapa762@gmail.com>
2024-07-11 11:34:11 +0545

    added new shopping feature

---
+++ lib/models/shoe.dart
@@ -0,0 +1,11 @@
+class Shoe {
+ final String name;
+ final String description;
+ final String price;
+ final String imagePath;
+ 
+ Shoe({required this.name,
+required this.description,
+required this.price,
+required this.imagePath});
+}
--- lib/pages/shoppingpage.dart
+++ lib/pages/shoppingpage.dart
@@ -1,31 +1,59 @@
+import 'package:ecommerce_app/models/shoe.dart';
 import 'package:flutter/material.dart';
+
+import '../components/shoetile.dart';
 class Shoppingpage extends StatelessWidget {
   const Shoppingpage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-   children: [
-          Container(
-            padding:EdgeInsets.all(8),
-            margin:EdgeInsets.all(20),
-            
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(8),
+    return Center(
+      child: Column(
+       
+         children: [
+            Container(
               
+              width: 400,
+              height: 500,
+              padding:EdgeInsets.all(8),
+              margin:EdgeInsets.all(20),
+              
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(8),
+
+                
+              ),
+              child:/* Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Center(child: Text("Search")),
+                  Icon(Icons.search),
+                ],
+              ),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Center(child: Text("Search")),
-                Icon(Icons.search),
-              ],
-            ),
-          ),
-          Text("Hot Picks 🔥"),
-         
-        ],
+            Text("Hot Picks 🔥"),
+      
+            const SizedBox(height: 10,),*/
+            
+             
+            Expanded(
+              child:ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 4,
+                itemBuilder: (context,index){
+                Shoe shoe= Shoe(name: "Kyrie infinity",
+                 description:"Light weight Basketball Shoe",
+                  price:"£300",
+                   imagePath:"lib/images/kyrie.jpeg");
+                return ShoeTile(
+                  shoe: shoe,
+                );
+              }) )
+            )
+          ],
+          
+      ),
     );
   }
 }
