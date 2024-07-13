21d395204ebad3ffd19de1a01535b729d8c2fb9e
vinay <vinaythapa762@gmail.com>
2024-07-13 21:52:08 +0545

    added cart

---
+++ lib/models/cart.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import 'shoe.dart';
+
+class Cart extends ChangeNotifier{
+
+  List<Shoe> Shop=[
+    Shoe(
+    name: "kyrie Infinity", 
+    description:"better agility",
+    price: "£300",
+    imagePath:'lib/images/Kyrie.jpeg'),
+     Shoe(
+    name: "Kai Anta", 
+    description:"better design",
+    price: "£200",
+    imagePath:'lib/images/Kai.jpeg'),
+     Shoe(
+    name: "nike KD 13", 
+    description:"better for ankles",
+    price: "£350",
+    imagePath:'lib/images/nike.png'),
+     Shoe(
+    name: "PG 6", 
+    description:"smooth performance",
+    price: "£400",
+    imagePath:'lib/images/pg.jpeg')
+
+  ];
+
+  List<Shoe> UserCart=[];
+
+  List<Shoe> getShoeList(){
+  return Shop;
+  }
+
+  List<Shoe> getUserCart(){
+    return UserCart;
+  }
+
+  void addItemToCart(Shoe shoe){
+    UserCart.add(shoe);
+    notifyListeners();
+  }
+  void removeItemFromCart(Shoe shoe){
+   UserCart.remove(shoe);
+   notifyListeners();
+  }
+}
